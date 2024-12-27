f4e2ba0fc007850b029ce79cdf08b38044750a08
Xiaoy <3804595787@qq.com>
2024-12-27 22:24:01 +0800

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -36,8 +36,8 @@ const Login: React.FC = () => {
         e.preventDefault();
 
         const data:UserData = {
-            username:account,
-            password,
+            username:xiaoy,
+            password:123456,
         };
 
         try {
