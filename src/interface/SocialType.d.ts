a2809a7a444644554f620e44c4642c81cf238f43
Xiaoy <3804595787@qq.com>
2024-12-27 22:41:12 +0800

    Update SocialType.d.ts

--- src/interface/SocialType.d.ts
+++ src/interface/SocialType.d.ts
@@ -1,5 +1,5 @@
 export interface SocialType{
-    socialGithub: https://github.com/xiaoy-awa;
+    socialGithub: string;
     socialEmail: string;
     socialBilibili: string;
     socialQQ: string;
